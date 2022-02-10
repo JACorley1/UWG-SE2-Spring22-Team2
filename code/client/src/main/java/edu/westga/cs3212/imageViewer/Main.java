cf62bad4ca03784029169186fb8f14dbe8d5ff83
Anton Maynard <amaynar2@my.westga.edu>
2022-02-10 13:49:27 -0500

    Add folder structure, made maven project

---
+++ code/client/src/main/java/edu/westga/cs3212/imageViewer/Main.java
@@ -0,0 +1,50 @@
+package edu.westga.cs3211.discountFinder;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * Entry point for the program
+ *
+ * @author	CS 3211
+ * @version Fall 2021
+ */
+public class Main extends Application {
+	public static final String WINDOW_TITLE = "Image Viewer";
+	public static final String GUI_RESOURCE = "view/codebehind/Login.fxml";
+
+	/**
+	 * JavaFX entry point.
+	 *
+	 * @precondition none
+	 * @postcondition none
+	 *
+	 * @throws IOException
+	 */
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent parent = FXMLLoader.load(getClass().getResource(Main.GUI_RESOURCE));
+		Scene scene = new Scene(parent);
+		primaryStage.setTitle(WINDOW_TITLE);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	/**
+	 * Primary Java entry point.
+	 *
+	 * @precondition none
+	 * @postcondition none
+	 *
+	 * @param args
+	 *            command line arguments
+	 */
+	public static void main(String[] args) {
+		Main.launch(args);
+	}
+}
