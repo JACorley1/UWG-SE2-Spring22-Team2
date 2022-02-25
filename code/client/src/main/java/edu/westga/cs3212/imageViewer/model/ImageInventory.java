460954dfb16a4bc589ed0aec90a60c32e7663cec
JaneraSmith <jsmit189@my.westga.edu>
2022-02-25 18:58:54 -0500

    Made additions to ImageInventory class for unit tests

--- code/client/src/main/java/edu/westga/cs3212/imageViewer/model/ImageInventory.java
+++ code/client/src/main/java/edu/westga/cs3212/imageViewer/model/ImageInventory.java
@@ -3,30 +3,60 @@ package edu.westga.cs3212.imageViewer.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The ImageInventory class.
+ * 
+ * @author Oumar Diallo & Janera Smith
+ * @version Spring 2022
+ *
+ */
 public class ImageInventory {
 	
 	private List<Image> images;
 	
+	/**
+	 * Instantiates a new ImageInventory
+	 * 
+	 * @precondition none
+	 * @postcondition size() == 0
+	 */
 	public ImageInventory() {
 		this.images = new ArrayList<Image>();
 	}
 	
 	/**
 	 * Adds the image.
-	 *
+	 * 
+	 * @precondition picture != null
+	 * @postcondition size() == size()@prev + 1
 	 * @param picture the picture
 	 */
 	public void addImage(Image picture) {
+		if (picture == null) {
+			throw new IllegalArgumentException("The picture cannot be null");
+		}
+		
 		this.images.add(picture);
 	}
 	
 	/**
 	 * Removes the image.
 	 *
+	 * @precondition picture != null
+	 * @postcondition size()
 	 * @param picture the picture
 	 */
 	public void removeImage(Image picture) {
 		this.images.remove(picture);
+	}
+	
+	/**
+	 * Gets the size of the images
+	 * 
+	 * @return the size of the images
+	 */
+	public int size() {
+		return this.images.size();
 	}
 
 }
