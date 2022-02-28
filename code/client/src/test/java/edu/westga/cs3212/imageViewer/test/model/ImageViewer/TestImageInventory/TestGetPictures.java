6a6b8476d01ab226b25ba125008687c015c826f2
JaneraSmith <jsmit189@my.westga.edu>
2022-02-27 22:50:05 -0500

    Added and/or made changes to unit tests

--- code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestImageInventory/TestGetPictures.java
+++ code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestImageInventory/TestGetPictures.java
@@ -11,67 +11,66 @@ import edu.westga.cs3212.imageViewer.model.ImageInventory;
 import edu.westga.cs3212.imageViewer.model.Picture;
 import javafx.scene.image.Image;
 
-class TestRemoveImage {
-
-	@Test
-	void testNullImage() {
-		ImageInventory inventory = new ImageInventory();
-		assertThrows(IllegalArgumentException.class, () -> {
-			inventory.removeImage(null);
-		});
-	}
+class TestGetPictures {
 	
 	@Test
-	void testRemoveOneImage() {
+	void testGetZeroPictures() {
+		ImageInventory inventory = new ImageInventory();
+		
+		assertEquals(0, inventory.getPictures().size());
+	}
+	@Test
+	void testGetOnePicture() {
 		ImageInventory inventory = new ImageInventory();
 		
 		FileInputStream inputStream;
+		
 		try {
 			inputStream = new FileInputStream("Assets/upload.jpg");
-			Image image = new Image(inputStream);
-			Picture picture = new Picture(image, "Click to upload image");
-			inventory.addImage(picture);
-			inventory.removeImage(picture);
 			
-			assertEquals(0, inventory.size());
+			Image image = new Image(inputStream);
+			
+			
+			Picture picture = new Picture(image, "Click to upload image");
+			
+			inventory.addImage(picture);
+			inventory.getPictures().add(image);
+			
+			assertEquals(1, inventory.getPictures().size());
 		} catch (FileNotFoundException exception) {
 			exception.printStackTrace();
 		}
 	}
 	
 	@Test
-	void testRemoveCoupleImages() {
+	void testGetSeveralPictures() {
 		ImageInventory inventory = new ImageInventory();
 		
 		FileInputStream inputStream;
 		FileInputStream inputStream2;
 		FileInputStream inputStream3;
-		FileInputStream inputStream4;
 		try {
 			inputStream = new FileInputStream("Assets/upload.jpg");
 			inputStream2 = new FileInputStream("Assets/upload.jpg");
 			inputStream3 = new FileInputStream("Assets/upload.jpg");
-			inputStream4 = new FileInputStream("Assets/upload.jpg");
 			
 			Image image = new Image(inputStream);
 			Image image2 = new Image(inputStream2);
 			Image image3 = new Image(inputStream3);
-			Image image4 = new Image(inputStream4);
 			
 			Picture picture = new Picture(image, "Click to upload image");
 			Picture picture2 = new Picture(image2, "Click to upload image");
 			Picture picture3 = new Picture(image3, "Click to upload image");
-			Picture picture4 = new Picture(image4, "Click to upload image");
 			
 			inventory.addImage(picture);
 			inventory.addImage(picture2);
 			inventory.addImage(picture3);
-			inventory.addImage(picture4);
 			
-			inventory.removeImage(picture);
-			inventory.removeImage(picture4);
+			inventory.getPictures().add(image);
+			inventory.getPictures().add(image2);
+			inventory.getPictures().add(image3);
 			
-			assertEquals(2, inventory.size());
+			assertEquals(3, inventory.getPictures().size());
 		} catch (FileNotFoundException exception) {
 			exception.printStackTrace();
 		}
