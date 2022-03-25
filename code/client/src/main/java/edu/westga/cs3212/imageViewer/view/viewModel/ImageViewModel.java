54810a7ec1dc77444cc0abecc2c293a9713c81f4
JaneraSmith <jsmit189@my.westga.edu>
2022-03-24 23:10:16 -0400

    Added ImageViewModel class inside view model package to modify add/upload functionality and add delete functionality

---
+++ code/client/src/main/java/edu/westga/cs3212/imageViewer/view/viewModel/ImageViewModel.java
@@ -0,0 +1,100 @@
+package edu.westga.cs3212.imageViewer.view.viewModel;
+
+import edu.westga.cs3212.imageViewer.model.ImageInventory;
+import edu.westga.cs3212.imageViewer.model.LoginManager;
+import edu.westga.cs3212.imageViewer.model.Picture;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ImageViewModel {
+	private final ListProperty<Image> pictureListProperty;
+	private final ObjectProperty<Picture> selectedPictureProperty;
+	private final ObjectProperty<Image> imageProperty;
+	private final StringProperty titleProperty;
+	
+	private ImageInventory imageInventory;
+	private ImageView imageView;
+	
+	
+	public ImageViewModel() {
+		
+		this.selectedPictureProperty = new SimpleObjectProperty<Picture>();
+		this.imageProperty = new SimpleObjectProperty<Image>();
+		this.titleProperty = new SimpleStringProperty();
+		
+		this.imageInventory = new ImageInventory();
+		this.imageView = new ImageView();
+		this.pictureListProperty = new SimpleListProperty<Image>();
+	}
+
+	/**
+	 * Gets the picture list property
+	 * 
+	 * @return the picture list property
+	 */
+	public ListProperty<Image> getPictureListProperty() {
+		return this.pictureListProperty;
+	}
+
+	/**
+	 * Gets the selected picture property
+	 * 
+	 * @return the selected picture property
+	 */
+	public ObjectProperty<Picture> getSelectedPictureProperty() {
+		return this.selectedPictureProperty;
+	}
+	
+	public ObjectProperty<Image> getImageProperty() {
+		return this.imageProperty;
+	}
+	
+	/**
+	 * Gets the title property
+	 * 
+	 * @return the title property
+	 */
+	public StringProperty getTitleProperty() {
+		return this.titleProperty;
+	}
+	
+	public void addPicture() {
+		Image newImage = this.imageProperty.get();
+    	//System.out.println(newImage.getUrl());
+    	String imageName = this.titleProperty.getValue();
+    	Picture.imageId += 1;
+    	Picture test = new Picture(newImage, imageName);
+    	this.imageInventory.addImage(test);
+    	System.out.println(test);
+    	LoginManager.loggedInUser.addImage(test);
+	}
+	
+//	/**
+//	 * Deletes a picture from the view
+//	 * 
+//	 * @precondition none
+//	 * @postcondition a picture has been deleted
+//	 * 
+//	 * @return true if a picture has been deleted; false otherwise
+//	 */
+//	public boolean deletePicture() {
+//		Image img = this.imageView.imageProperty().get();
+//		String title = this.titleProperty.getValue();
+//		Picture picture = new Picture(img, title);
+//		
+//		if (picture != null) {
+//			this.imageInventory.removeImage(picture);
+//			this.pictureListProperty.set(FXCollections.observableArrayList(this.imageInventory.getPictures()));
+//		}
+//		
+//		return false;
+//	}
+}
