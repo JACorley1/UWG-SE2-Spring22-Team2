c6cdc4f023032905630dd910f056720e8fa3b350
JaneraSmith <jsmit189@my.westga.edu>
2022-04-12 11:45:00 -0400

    Fixed the test error for the TestAddImage test class from User class due to changes.

--- code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestUser/TestAddImage.java
+++ code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestUser/TestAddImage.java
@@ -1,6 +1,6 @@
 package edu.westga.cs3212.imageViewer.test.model.ImageViewer.TestUser;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import edu.westga.cs3212.imageViewer.model.Picture;
 import edu.westga.cs3212.imageViewer.model.User;
-import javafx.scene.image.Image;
 
 class TestAddImage {
 
@@ -20,8 +19,8 @@ class TestAddImage {
 		FileInputStream inputStream;
 		try {
 			inputStream = new FileInputStream("Assets/upload.jpg");
-			Image image = new Image(inputStream);
-			Picture picture = new Picture(image, "Click to upload image");
+			//Image image = new Image(inputStream);
+			Picture picture = new Picture(inputStream, "Click to upload image", 1);
 			
 			assertEquals(true, user.addImage(picture));
 		} catch (FileNotFoundException exception) {
