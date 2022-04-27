083843f5f2ff819b72c4ab2852e9881a94999f89
JaneraSmith <jsmit189@my.westga.edu>
2022-04-27 18:44:29 -0400

    Made modifications to TestAddImage test class due to changes to Picture class

--- code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestImageInventory/TestAddImage.java
+++ code/client/src/test/java/edu/westga/cs3212/imageViewer/test/model/ImageViewer/TestImageInventory/TestAddImage.java
@@ -1,15 +1,15 @@
 package edu.westga.cs3212.imageViewer.test.model.ImageViewer.TestImageInventory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.Test;
 
-import edu.westga.cs3212.imageViewer.model.Picture;
-import javafx.scene.image.Image;
 import edu.westga.cs3212.imageViewer.model.ImageInventory;
+import edu.westga.cs3212.imageViewer.model.Picture;
 
 class TestAddImage {
 
@@ -27,7 +27,6 @@ class TestAddImage {
 		FileInputStream inputStream;
 		try {
 			inputStream = new FileInputStream("Assets/upload.jpg");
-			Image image = new Image(inputStream);
 			Picture picture = new Picture(inputStream, "Click to upload image", 1);
 			inventory.addImage(picture);
 			
